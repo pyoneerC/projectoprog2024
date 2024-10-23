deaf2303a26a2af986c5ee0f5886515779c600bd
pyoneerC <maxcomperatutti@gmail.com>
2024-10-23 14:13:46 -0300

    variables

--- Source/projectoprog2024/projectoprog2024Character.h
+++ Source/projectoprog2024/projectoprog2024Character.h
@@ -69,5 +69,11 @@ public:
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	/** Returns FollowCamera subobject **/
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay")
+    int32 Coins;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay")
+    bool bIsAlive;
 };
 
